886a5c3d04344f836d5dd5c3857c231d3482b5ad
Poonam Sagar <poonamsagar9212@gmail.com>
2019-03-23 11:27:51 +1100

    Updates

---
+++ precache-manifest.7a95b374e012ea901da789d2cc87fb00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "040a3671a07ec194947f2f2c34fcc9dd",
+    "url": "/poonamsagar/myProfile/static/media/pic.jpg.040a3671.jpeg"
+  },
+  {
+    "revision": "54c7f1631658b5f3e13a",
+    "url": "/poonamsagar/myProfile/static/js/runtime~main.54c7f163.js"
+  },
+  {
+    "revision": "b272b542a074c75bd17f",
+    "url": "/poonamsagar/myProfile/static/js/main.b272b542.chunk.js"
+  },
+  {
+    "revision": "7cfaf6fe947c45d13e89",
+    "url": "/poonamsagar/myProfile/static/js/1.7cfaf6fe.chunk.js"
+  },
+  {
+    "revision": "b272b542a074c75bd17f",
+    "url": "/poonamsagar/myProfile/static/css/main.30e7b9d5.chunk.css"
+  },
+  {
+    "revision": "7cfaf6fe947c45d13e89",
+    "url": "/poonamsagar/myProfile/static/css/1.5255f862.chunk.css"
+  },
+  {
+    "revision": "09ff930740f20e96b4e6eb85aa529d85",
+    "url": "/poonamsagar/myProfile/index.html"
+  }
+];
